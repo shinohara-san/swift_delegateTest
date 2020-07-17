9ec866ff1ee5e5a817d70bb0bc51cc8a8eac9eb5
shinohara-san <someiyoshino0809@gmail.com>
2020-07-17 17:14:00 +0900

    first

--- delegateTest/ViewController.swift
+++ delegateTest/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, TestDelegate {
+///このVCはUIViewControllerとTestDelegateに準拠
+///TestDelegateに準拠→要件を守る必要(この場合, testという関数を持つこと)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let testClass = TestClass()
+        testClass.delegate = self
+        testClass.delegateTest()
+///TestClassインスタンス化→delegateをViewControllerに設定→処理をVCで行う(確認用。プリントされる)
     }
-
-
+    
+    func test(string: String) {
+      print(string)
+    }
+///TestDelegateに準拠するために上記関数を設置(何もアクションもないが、ないとエラーになる)
 }
 
+protocol TestDelegate: class {
+    func test(string: String)
+}
+///TestDelegateというプロトコル(準拠するならこれらの変数や関数を持つ必要があるという決まり)
+///TestDelegateに準拠するなら、testという文字列を引数にとる関数が必要
+
+class TestClass {
+    weak var delegate: TestDelegate?
+    func delegateTest() {
+        delegate?.test(string: "test メソッドが実行されたという通知を受け取る")
+    }
+///TestDelegateであるdelegateという変数を持つTestClassというクラス
+///
+}
